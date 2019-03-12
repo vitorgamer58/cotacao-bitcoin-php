eaa8550c95578b06ad674dbdcd7a968cae6698ca
Vitor Araujo <vitorgamer58@outlook.com>
2019-03-12 12:22:11 -0300

    Numeros arredondados
    Retirada a função INTVAL que arredondava os numeros para cima

--- run.php
+++ run.php
@@ -7,8 +7,8 @@ $jsonbraziliex = file_get_contents("https://braziliex.com/api/v1/public/ticker/b
 $databrzx = json_decode($jsonbraziliex, true); //decodifica os dados da api
 $braziliex_price = $databrzx['last']; //seleciona um valor especifico da api
 $braziliex_volume = $databrzx['baseVolume'];
-$braziliex_price = intval($braziliex_price); //transforma em numero
-$braziliex_volume = intval($braziliex_volume);
+//$braziliex_price = intval($braziliex_price); //transforma em numero
+//$braziliex_volume = intval($braziliex_volume);
 $varbraziliex = $braziliex_price * $braziliex_volume;
 
 
@@ -17,8 +17,8 @@ $json_bitcointrade = file_get_contents("https://api.bitcointrade.com.br/v1/publi
 $data_bitcoin_trade = json_decode($json_bitcointrade, true);
 $bitcointrade_price = $data_bitcoin_trade['data']['last'];
 $bitcointrade_volume = $data_bitcoin_trade['data']['volume'];
-$bitcointrade_price = intval($bitcointrade_price);
-$bitcointrade_volume = intval($bitcointrade_volume);
+//$bitcointrade_price = intval($bitcointrade_price);
+//$bitcointrade_volume = intval($bitcointrade_volume);
 $varbitcointrade = $bitcointrade_price * $bitcointrade_volume;
 
 //walltime
@@ -26,8 +26,8 @@ $json_walltime = file_get_contents("https://s3.amazonaws.com/data-production-wal
 $datawalltime = json_decode($json_walltime, true);
 $walltime_price = $datawalltime['BRL_XBT']['last_inexact'];
 $walltime_volume = $datawalltime['BRL_XBT']['quote_volume24h_inexact'];
-$walltime_price = intval($walltime_price);
-$walltime_volume = intval($walltime_volume);
+//$walltime_price = intval($walltime_price);
+//$walltime_volume = intval($walltime_volume);
 $varwalltime = $walltime_price * $walltime_volume;
 
 //bitcointoyou
@@ -35,8 +35,8 @@ $json_bitcointoyou = file_get_contents("https://www.bitcointoyou.com/api/ticker.
 $databitcointoyou = json_decode($json_bitcointoyou, true);
 $bitcointoyou_price = $databitcointoyou['ticker']['last'];
 $bitcointoyou_volume = $databitcointoyou['ticker']['vol'];
-$bitcointoyou_price = intval($bitcointoyou_price);
-$bitcointoyou_volume = intval($bitcointoyou_volume);
+//$bitcointoyou_price = intval($bitcointoyou_price);
+//$bitcointoyou_volume = intval($bitcointoyou_volume);
 $varbitcointoyou = $bitcointoyou_price * $bitcointoyou_volume;
 
 //mercadobitcoin
@@ -44,8 +44,8 @@ $json_mercadobitcoin = file_get_contents("https://www.mercadobitcoin.net/api/btc
 $datamercadobitcoin = json_decode($json_mercadobitcoin, true);
 $mercadobitcoin_price = $datamercadobitcoin['ticker']['last'];
 $mercadobitcoin_volume = $datamercadobitcoin['ticker']['vol'];
-$mercadobitcoin_price = intval($mercadobitcoin_price);
-$mercadobitcoin_volume = intval($mercadobitcoin_volume);
+//$mercadobitcoin_price = intval($mercadobitcoin_price);
+//$mercadobitcoin_volume = intval($mercadobitcoin_volume);
 $varmercadobitcoin = $mercadobitcoin_price * $mercadobitcoin_volume;
 
 //flowbtc
@@ -53,8 +53,8 @@ $json_flowbtc = file_get_contents("https://publicapi.flowbtc.com.br/v1/ticker/bt
 $dataflowbtc = json_decode($json_flowbtc, true);
 $flowbtc_price = $dataflowbtc['data']['LastTradedPx'];
 $flowbtc_volume = $dataflowbtc['data']['Rolling24HrVolume'];
-$flowbtc_price = intval($flowbtc_price);
-$flowbtc_volume = intval($flowbtc_volume);
+//$flowbtc_price = intval($flowbtc_price);
+//$flowbtc_volume = intval($flowbtc_volume);
 $varflowbtc = $flowbtc_price * $flowbtc_volume;
 
 
@@ -62,10 +62,11 @@ $varflowbtc = $flowbtc_price * $flowbtc_volume;
 $allvariables = $varbraziliex + $varbitcointrade + $varwalltime + $varbitcointoyou + $varmercadobitcoin + $varflowbtc; //soma todas as variaveis
 
 $volumetotal = $braziliex_volume + $bitcointrade_volume + $walltime_volume + $bitcointoyou_volume + $mercadobitcoin_volume + $flowbtc_volume; //soma todos os volumes
+$volumetotal = round($volumetotal, 8); //Bitcoin tem 8 casas decimais
 
 $preco_ponderado = $allvariables / $volumetotal; //calcula o preco medio ponderado
 
-$preco_ponderado = intval($preco_ponderado); //transforma em numero
+//$preco_ponderado = intval($preco_ponderado); //transforma em numero
 
 //Calcula o MarketShare
 $pbraziliex = round(($braziliex_volume/$volumetotal)*100, 2);
